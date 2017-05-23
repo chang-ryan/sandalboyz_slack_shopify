936478486ec8b618177cebfd73593682c02d4bfd
Ryan Chang <ryan.chang@hyfn.com>
2017-05-23 15:00:36 -0700

    create routes

--- app/controllers/v1/inventory_controller.rb
+++ app/controllers/v1/inventory_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class InventoryController < ApplicationController
+  def index; end
+
   def create
     render json: inventory
   end
+
+  private
+
+  def permitted_params; end
 end
