dacde631c382cee56523f251fa1ac091203c0ccc
Ryan Chang <ryan.chang@hyfn.com>
2017-05-23 16:39:37 -0700

    create product variant

--- db/migrate/20170523233008_create_product_variants.rb
+++ db/migrate/20170523233008_create_product_variants.rb
@@ -1,6 +1,7 @@
 class CreateProductVariants < ActiveRecord::Migration[5.1]
   def change
     create_table :product_variants do |t|
+      t.references :product, foreign_key: true, null: false
       t.string :name,      null: false
       t.string :remote_id, null: false
       t.string :sku,       null: false
