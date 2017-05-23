0bf3326069740b6fa0cb3691d28e874230b3f180
Ryan Chang <ryan.chang@hyfn.com>
2017-05-23 16:39:37 -0700

    create product variant

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  has_many :product_variants
+  alias variants product_variants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523070808) do
+ActiveRecord::Schema.define(version: 20170523233008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_variants", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.string "name", null: false
+    t.string "remote_id", null: false
+    t.string "sku", null: false
+    t.integer "inventory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_variants_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema.define(version: 20170523070808) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "product_variants", "products"
 end
